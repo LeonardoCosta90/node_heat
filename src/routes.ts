de1f880f8a1a750032561549190002ee3cbdee54
Leonardo Costa <leonardo.costa@toodoo.com.br>
2021-10-23 14:28:19 -0300

    feat: finish project

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,23 @@
-import { Router } from 'express';
-import AuthenticateUserController from './controllers/authenticate-user-controller';
+
+import { Router } from "express";
+import AuthenticateUserController from "./controllers/authenticate-user-controller";
+import { CreateMessageController } from "./controllers/create-message-controller";
+import { Get3LastMessagesController } from "./controllers/get-last3-messages-controller";
+import { ProfileUserController } from "./controllers/profile-user-controller";
+import { ensureAuthenticated } from "./middleware/ensure-authenticated";
 
 const router = Router();
 
-router.post('/authenticate', new AuthenticateUserController().handle);
+router.post("/authenticate", new AuthenticateUserController().handle);
 
-export default router;
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
+
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
+export { router };
